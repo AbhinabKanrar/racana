809d44fde11cfef4e42c7de5e7cf85b0021ba154
abhinab <abhinabkanrar@gmail.com>
2018-07-21 21:41:30 +0530

    adding gulp

--- racana-ui/gulpfile.js
+++ racana-ui/gulpfile.js
@@ -1,34 +1,41 @@
 "use strict"
 
 const gulp = require('gulp'),
-  cleanCSS = require('gulp-clean-css'),
-  uglify = require('gulp-uglify'),
-  pump = require('pump')
+    cleanCSS = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    htmlmin = require('gulp-htmlmin'),
+    pump = require('pump')
 
-const srcCss = 'public/stylesheet/**/*.css',
-  srcJs = 'public/scripts/**/*.js',
-  srcFont = 'public/fonts/**/*.woff2'
+var srcCss = 'public/stylesheet/**/*.css',
+    srcJs = 'public/scripts/**/*.js',
+    srcFont = 'public/fonts/**/*.woff2',
+    srcHtml = 'public/**/*.html'
 
-const destCss = 'dist/stylesheet',
-  destJs = 'dist/scripts',
-  destFont = 'dist/fonts'
+var destCss = 'dist/stylesheet',
+    destJs = 'dist/scripts',
+    destFont = 'dist/fonts',
+    descHtml = 'dist'
 
 gulp.task('css', function () {
-  return gulp.src(srcCss).pipe(cleanCSS({ compatibility: 'ie8' })).pipe(gulp.dest(destCss))
+    return gulp.src(srcCss).pipe(cleanCSS({ compatibility: 'ie8' })).pipe(gulp.dest(destCss))
 })
 
 gulp.task('js', function (cb) {
-  pump([
-    gulp.src(srcJs),
-    uglify(),
-    gulp.dest(destJs)
-  ], cb)
+    pump([
+        gulp.src(srcJs),
+        uglify(),
+        gulp.dest(destJs)
+    ], cb)
 })
 
 gulp.task('font', function () {
-  return gulp.src(srcFont).pipe(gulp.dest(destFont))
+    return gulp.src(srcFont).pipe(gulp.dest(destFont))
 })
 
-gulp.task('default', gulp.series(['css', 'js', 'font'], function (done) {
-  done()
+gulp.task('html', function () {
+    return gulp.src(srcHtml).pipe(htmlmin({ collapseWhitespace: true })).pipe(gulp.dest(descHtml))
+})
+
+gulp.task('default', gulp.series(['css', 'js', 'font', 'html'], function (done) {
+    done()
 }))
