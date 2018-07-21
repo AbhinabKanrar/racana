2c90b72db4b9252162d446f8a8f98adbf809cc79
abhinab <abhinabkanrar@gmail.com>
2018-07-21 22:01:51 +0530

    adding react

--- racana-ui/app.js
+++ racana-ui/app.js
@@ -7,6 +7,10 @@ const app = express()
 const path = require("path")
 
 app.use(express.static(path.join(__dirname, 'dist')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.get('*', (req, res) => res.render('/dist/login'))
 
 server.init(app)
-
